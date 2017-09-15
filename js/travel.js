a691a82beb7762f6916d3f013048198a9bc82288
andyymontenegro <ajrunner10@gmail.com>
2017-09-15 01:16:44 -0500

    extend firebase init

--- js/travel.js
+++ js/travel.js
@@ -1,4 +1,14 @@
 // Initialize Firebasae
-
+var config = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
+  };
+  firebase.initializeApp(config);
+  
+  var database = firebase.database();
 // global variables 
     // --------------------------
